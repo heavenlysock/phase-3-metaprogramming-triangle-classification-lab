f5469c96a6a437375522cb7aa91be9f42938d81a
bobertsworld <bobertsworld@gmail.com>
2023-01-11 12:02:43 -0800

    commit tree 19ff9f1b1c203df5b23676a1491fee107c8ac347

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+  def initialize(sidea, sideb, sidec)
+
+    @a = sidea
+    @b = sideb
+    @c = sidec
+
+  end
+  def validate_triangle
+    if 0 >= (a) || 0 >= (b) || 0 >= (c) || (a + b) <= (c) || (a + c) <= (b) || (b + c) <= (a)
+    raise TriangleError
+    end
+  end
+  def kind
+    validate_triangle
+    if a == b && b == c
+      return :equilateral
+    elsif a == b || a == c || b == c
+      return :isosceles
+    else 
+      return :scalene
+    end
+  end
+  # def a
+  #   @a
+  # end
+  # def b
+  #   @b
+  # end
+  # def c
+  #   @c
+  # end
+  class TriangleError < StandardError
+    # triangle error code
+  end
 end
